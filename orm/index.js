192c4e9b91165df37d3933cb5bf3af570026d254
Ben Roznos <benrozos@gmail.com>
2020-06-01 14:16:41 -0700

    Added orm file

---
+++ orm/index.js
@@ -0,0 +1,34 @@
+const db = require('../db')
+
+module.exports = {
+  getAll(table, cb) {
+    db.query(`SELECT * FROM ${table}`, (err, data) => {
+      if (err) { console.log(err) }
+      cb(data)
+    })
+  },
+  getAllWhere(table, where, cb) {
+    db.query(`SELECT * FROM ${table} WHERE ?`, where, (err, data) => {
+      if (err) { console.log(err) }
+      cb(data)
+    })
+  },
+  createOne(table, data, cb) {
+    db.query(`INSERT INTO ${table} SET ?`, data, (err, info) => {
+      if (err) { console.log(err) }
+      cb(info)
+    })
+  },
+  updateOne(table, changes, where, cb) {
+    db.query(`UPDATE ${table} SET ? WHERE ?`, [changes, where], (err, info) => {
+      if (err) { console.log(err) }
+      cb(info)
+    })
+  },
+  deleteOne(table, where, cb) {
+    db.query(`DELETE FROM ${table} WHERE ?`, where, (err, info) => {
+      if (err) { console.log(err) }
+      cb(info)
+    })
+  }
+}
