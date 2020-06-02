a105d28cf23906f4b857a8320e16df4d2f658191
Jimmy Echternach <jimmynechternach@gmail.com>
2020-06-02 14:05:14 -0700

    updated db

--- sql/seed.sql
+++ sql/seed.sql
@@ -3,10 +3,10 @@ USE moSQL_db;
 INSERT INTO users (username, password)
 VALUES ("jimmy", "Password1"), ("Ben", "anotherPassword"), ("Michael", "differentPassword"), ("Kele", "lastPassword");
 
-INSERT INTO moviePost (userId, title, director, genre, starring, plot, mpaaRating, postBody, dateTime)
-VALUES (1, "Guardians of the Galaxy Vol. 2","James Gunn","Action, Adventure, Comedy, Sci-Fi","Chris Pratt, Zoe Saldana, Dave Bautista, Vin Diesel", "movie plot", "PG-13","I really liked this movie","June 1st 2020, 2:15:33 pm"),
-(2, "Alien", "Ridley ScmoviePostott", "Horror, Sci-Fi", "Tom Skerritt, Sigourney Weaver, Veronica Cartwright, Harry Dean Stanton", "movie plot", "R", "Super scary movie. What do you guys think?", "June 1st 2020, 2:20:35 pm"),
-(3, "Trainspotting", "Danny Boyle", "Drama", "Ewan McGregor, Ewen Bremner, Jonny Lee Miller, Kevin McKidd", "movie plot", "R", "The only good movie Ewan McGregor has made", "June 1st 2020, 2:32:13 pm");
+INSERT INTO moviePost (userId, title, poster, year, director, genre, starring, plot, mpaaRating, postBody, dateTime)
+VALUES (1, "Guardians of the Galaxy Vol. 2", "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg", "2017", "James Gunn","Action, Adventure, Comedy, Sci-Fi","Chris Pratt, Zoe Saldana, Dave Bautista, Vin Diesel", "movie plot", "PG-13","I really liked this movie","June 1st 2020, 2:15:33 pm"),
+(2, "Alien", "https://m.media-amazon.com/images/M/MV5BMmQ2MmU3NzktZjAxOC00ZDZhLTk4YzEtMDMyMzcxY2IwMDAyXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg", "1979", "Ridley Scott", "Horror, Sci-Fi", "Tom Skerritt, Sigourney Weaver, Veronica Cartwright, Harry Dean Stanton", "movie plot", "R", "Super scary movie. What do you guys think?", "June 1st 2020, 2:20:35 pm"),
+(3, "Trainspotting", "https://m.media-amazon.com/images/M/MV5BMzA5Zjc3ZTMtMmU5YS00YTMwLWI4MWUtYTU0YTVmNjVmODZhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg", "1996", "Danny Boyle", "Drama", "Ewan McGregor, Ewen Bremner, Jonny Lee Miller, Kevin McKidd", "movie plot", "R", "The only good movie Ewan McGregor has made", "June 1st 2020, 2:32:13 pm");
 
 
 INSERT INTO comments (userId, moviePostId, body)
