9092abe9145295d31bcec212b2917db78c0d8578
Ben Roznos <benrozos@gmail.com>
2020-06-03 14:28:28 -0700

    adding login/logout code withy nav bar

--- public/movie.js
+++ public/movie.js
@@ -5,11 +5,64 @@
 http://www.omdbapi.com/?t=remember_the_titans&apikey=764b0a16
 
 // ---------------------------------------------------------------------------
+// signUp card hide /show
+function hideSignUp(){
+  if (document.getElementById('signUp').classList[1] === 'hide' && document.getElementById('login').classList[1] === 'hide') {
+    document.getElementById('signUp').classList.remove('hide')}
+  else if (document.getElementById('signUp').classList[1] === 'hide' && document.getElementById('login').classList[1] !== 'hide') {
+    return}
+  else {
+    document.getElementById('signUp').classList.add('hide')}}
 
-document.getElementById('searchMovie').addEventListener('click', event => {
+// login card hide/show
+function hideLogin(){
+  if (document.getElementById('login').classList[1] === 'hide' && document.getElementById('signUp').classList[1] === 'hide') {
+    document.getElementById('login').classList.remove('hide')}
+  else if (document.getElementById('login').classList[1] === 'hide' && document.getElementById('signUp').classList[1] !== 'hide') {
+    return}
+  else {
+    document.getElementById('login').classList.add('hide')}}
+
+  // removes login and signup buttons and displays username(given) and a log out button
+  function displayUser(username){
+    document.getElementById('loginBtn').classList.add('hide')
+    document.getElementById('signUpBtn').classList.add('hide')
+    document.getElementById('userDisp').classList.remove('hide')
+    document.getElementById('logOutBtn').classList.remove('hide')
+    document.getElementById('userDisp').innerHTML=`Hello ${username}`
+    username = document.getElementById('email').value =''
+    document.getElementById('first_name').value = ''
+    document.getElementById('last_name').value = ''
+  }
+  function logOut(){
+    document.getElementById('loginBtn').classList.remove('hide')
+    document.getElementById('signUpBtn').classList.remove('hide')
+    document.getElementById('userDisp').classList.add('hide')
+    document.getElementById('logOutBtn').classList.add('hide')
+    document.getElementById('userDisp').innerHTML = ``
+  }
+// event listeners for sign up and login
+document.getElementById('signUpBtn').addEventListener('click',event=>{hideSignUp()})
+document.getElementById('loginBtn').addEventListener('click', event => {hideLogin()})
+document.getElementById('logOutBtn').addEventListener('click', event =>{logOut()})
+// sign up new user
+document.getElementById('newUser').addEventListener('click', event =>{
   event.preventDefault()
-  console.log(document.getElementById('Movie').value)
-  let Search = document.getElementById('Movie').value
+  let firstName = document.getElementById('first_name').value
+  console.log(firstName)
+  let lastName = document.getElementById('last_name').value
+  console.log(lastName)
+  let username = document.getElementById('email').value
+  console.log(username)
+  hideSignUp()
+  displayUser(username)
+})
+// Takes input from the searchbar and runs it though ombd api to return movie cards w/ basic info and poster
+document.getElementById('searchBtn').addEventListener('click', event => {
+  event.preventDefault()
+  document.getElementById('newMovie').innerHTML= ''
+  console.log(document.getElementById('searchContent').value)
+  let Search = document.getElementById('searchContent').value
   let newSearch = ""
   for (let i = 0; i < Search.length; i++) {
     if (Search.charAt(i) === ' ') {
@@ -28,53 +81,53 @@ document.getElementById('searchMovie').addEventListener('click', event => {
       movieElem.className = 'card'
       movieElem.style = 'width: 18rem;'
       movieElem.innerHTML = `
-          <img 
-            src="${Data.Poster}" 
-            class="card-img-top" 
-            alt="${Data.Title}">
-          <div class="card-body">
-            <h5 class="card-title">
-              ${Data.Title}
-            </h5>
-            <ul class="list-group list-group-flush">
+    <div class="col s12 m7">
+      <div class="card">
+        <div class="card-image">
+          <img src="${Data.Poster}">
+          <span class="card-title grey darken-3 text-white">${Data.Title}</span>
+        </div>
+        <div class="card-content">
+          <ul class="list-group list-group-flush" id='movieCard>
               <li class="list-group-item">
-                ${Data.Actors}
+                <h6>Actors:</h6>
+                <p> ${Data.Actors}</p>
               </li>
               <li class="list-group-item">
-                ${Data.Director}
+                <h6>Director:</h6> 
+                <p>${Data.Director}</p>
               </li>
               <li class="list-group-item">
-                ${Data.Plot}
+                <h6>Plot:</h6> 
+                <p>${Data.Plot}</p>
               </li>
               <li class="list-group-item">
-                ${Data.Rated}
+                <h6>Rating:</h6> 
+                <p>${Data.Rated}</p>
               </li>
               <li class="list-group-item">
-                ${Data.Year}
+                <h6>Year:</h6> 
+                <p>${Data.Year}</p>
               </li>
             </ul>
-          </div>
+        </div>
+      </div>
+    </div>
+  </div>
         `
       document.getElementById('newMovie').append(movieElem)
+      document.getElementById('searchContent').value = ''
+
+      // next 8 lines refer to creating button links to the post for slected movies as well as a button for creating a post for movies that don't already have one
+
+      // if(newSearch already has a post){
+      //   let linkPost = `<li><a class="waves-effect waves-light btn" id='linkReq'>Go to post</a></li>`
+      //   document.getElementById('movieCard').append(addPost)
+      //   }
+      // else if (newSearch does not have a post){
+      //   let addPost = `<li><a class="waves-effect waves-light btn" id='newReq'>Make new post</a></li>`
+      //   document.getElementById('movieCard').append(addPost)
+      //   }
     })
     .catch(e => console.log(e))
 })
-
-
-// ---------------------------------------------------------------
-// // todo list example api04
-
-// const renderItems = () => {
-//   document.getElementById('items').innerHTML = ''
-//   for (let i = 0; i < items.length; i++) {
-//     let itemElem = document.createElement('li')
-//     itemElem.classList.add('list-group-item')
-//     itemElem.classList.add(items[i]._____ ? 'list-group-item-success' : 'list-group-item-secondary')
-//     itemElem.classList.add('d-flex', 'justify-content-between', 'align-items-center')
-//     itemElem.innerHTML = `
-//           ${items[i].____}
-
-//         `
-//     document.getElementById('items').append(itemElem)
-//   }
-// }
