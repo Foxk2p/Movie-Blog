ee9eda4c3cae83a867e985bdc0141341bf0779a7
Ben Roznos <benrozos@gmail.com>
2020-06-03 08:45:14 -0700

    updating to match master branch

--- public/movie.js
+++ public/movie.js
@@ -1,103 +1,80 @@
 
-OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=764b0a16
+// OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=764b0a16
 // or try trilogy as key
 
+http://www.omdbapi.com/?t=remember_the_titans&apikey=764b0a16
 
-// pokemon example api04
+// ---------------------------------------------------------------------------
 
 document.getElementById('searchMovie').addEventListener('click', event => {
   event.preventDefault()
   console.log(document.getElementById('Movie').value)
+  let Search = document.getElementById('Movie').value
+  let newSearch = ""
+  for (let i = 0; i < Search.length; i++) {
+    if (Search.charAt(i) === ' ') {
+      newSearch += '_'
+    }
+    else {
+      newSearch += Search.charAt(i)
+    }
+  }
 
-  fetch(`https://${document.getElementById('Movie').value}/`)
+  fetch(`http://www.omdbapi.com/?t=${newSearch}&apikey=764b0a16`)
     .then(r => r.json())
-    .then(movie => {
-      console.log(movie)
+    .then(Data => {
+      console.log(Data)
       let movieElem = document.createElement('div')
       movieElem.className = 'card'
       movieElem.style = 'width: 18rem;'
       movieElem.innerHTML = `
           <img 
-            src="${}" 
+            src="${Data.Poster}" 
             class="card-img-top" 
-            alt="${}">
+            alt="${Data.Title}">
           <div class="card-body">
             <h5 class="card-title">
-              ${}
+              ${Data.Title}
             </h5>
             <ul class="list-group list-group-flush">
               <li class="list-group-item">
-                ${}: ${}
+                ${Data.Actors}
               </li>
               <li class="list-group-item">
-                ${}: ${}
+                ${Data.Director}
               </li>
               <li class="list-group-item">
-                ${}: ${}
+                ${Data.Plot}
               </li>
               <li class="list-group-item">
-                ${}: ${}
+                ${Data.Rated}
               </li>
               <li class="list-group-item">
-                ${}: ${}
-              </li>
-              <li class="list-group-item">
-                ${}: ${}
+                ${Data.Year}
               </li>
             </ul>
           </div>
         `
-      document.getElementById('').append(movieElem)
+      document.getElementById('newMovie').append(movieElem)
     })
     .catch(e => console.log(e))
 })
 
 
-// --------------------------------
+// ---------------------------------------------------------------
+// // todo list example api04
 
+// const renderItems = () => {
+//   document.getElementById('items').innerHTML = ''
+//   for (let i = 0; i < items.length; i++) {
+//     let itemElem = document.createElement('li')
+//     itemElem.classList.add('list-group-item')
+//     itemElem.classList.add(items[i]._____ ? 'list-group-item-success' : 'list-group-item-secondary')
+//     itemElem.classList.add('d-flex', 'justify-content-between', 'align-items-center')
+//     itemElem.innerHTML = `
+//           ${items[i].____}
 
-// todo list example api04
-const renderItems = () => {
-  document.getElementById('items').innerHTML = ''
-  for (let i = 0; i < items.length; i++) {
-    let itemElem = document.createElement('li')
-    itemElem.classList.add('list-group-item')
-    itemElem.classList.add(items[i]._____ ? 'list-group-item-success' : 'list-group-item-secondary')
-    itemElem.classList.add('d-flex', 'justify-content-between', 'align-items-center')
-    itemElem.innerHTML = `
-          ${items[i].____}
-         
-        `
-    document.getElementById('items').append(itemElem)
-  }
-}
-
-// -----------------------------------------
-
-// starwars example api04
-
-fetch('')
-  .then(r => r.json())
-  .then(movie => {
-    console.log(movie)
-
-    fetch(movie._______)
-      .then(r => r.json())
-      .then(_______ => {
-        console.log(_______)
-        document.getElementById('movie').innerHTML = `
-          <div class="card-header">Star Wars</div>
-          <div class="card-body">
-            <h5 class="card-title">${movie.name}</h5>
-            <p class="card-text">
-            ${movie.title} text ${movie.cast} text ${movie.duration} text.
-            </p>
-            <p class="card-text">
-            text ${movie.director}.
-            </p>
-          </div>
-          `
-      })
-      .catch(e => console.log(e))
-  })
-  .catch(e => console.log(e))
+//         `
+//     document.getElementById('items').append(itemElem)
+//   }
+// }
--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const app = express()
 const mysql = require('mysql2')
 
 // not sure about db name, or if this is correct way to connect to db
-module.exports = mysql.createConnection('mysql://root:root@localhost:3306/moSQL_db')
+// module.exports = mysql.createConnection('mysql://root:root@localhost:3306/moSQL_db')
 
 // app.engine('.hbs', require('express-handlebars')({ extname: '.hbs' }))
+app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 // app.set('view engine', '.hbs')
 app.use(express.json())
@@ -18,4 +19,5 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.listen(3000, () => console.log('http://localhost:3000'))
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => { console.log(`Server running on port ${PORT}...`) })
