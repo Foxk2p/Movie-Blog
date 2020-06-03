b7f77943218dda9d0787a01d0e135ae6f0fbb5cf
K2P <foxk2p@gmail.com>
2020-06-03 10:42:47 -0700

    Sequalize db connection reference

---
+++ db/index.js
@@ -0,0 +1,7 @@
+// reference from sqlz01 repo
+
+// const Sequelize = require('sequelize')
+
+// const sequelize = new Sequelize('mysql://root:rootroot@localhost:3306/user_db')
+
+// module.exports = sequelize
