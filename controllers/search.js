93db79242227e9c21ef62fced50db250722ead7d
Jimmy Echternach <jimmynechternach@gmail.com>
2020-06-08 09:47:33 -0700

    moved api key to the backend

---
+++ controllers/search.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  searchMovies(req, res) {
+    
+      // .then(r => {
+      //   console.log(r)
+      //   return r
+      // })
+      
+      // .catch(e => console.log(e))
+  }
+}
--- public/movie.js
+++ public/movie.js
@@ -1,9 +1,4 @@
 
-// OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=764b0a16
-// or try trilogy as key
-
-http://www.omdbapi.com/?t=remember_the_titans&apikey=764b0a16
-
 // ---------------------------------------------------------------------------
 // signUp card hide /show
 function hideSignUp() {
@@ -88,8 +83,8 @@ document.getElementById('searchBtn').addEventListener('click', event => {
       newSearch += Search.charAt(i)
     }
   }
-
-  fetch(`http://www.omdbapi.com/?t=${newSearch}&apikey=764b0a16`)
+// instead of OMDB, we will fetch 'api/search/${newSearch}/${postBody}'
+  fetch(`http://www.omdbapi.com/?t=${newSearch}&apikey=${process.env.OMDBAPIKEY}`)
     .then(r => r.json())
     .then(Data => {
       console.log(Data)
@@ -141,7 +136,7 @@ document.getElementById('searchBtn').addEventListener('click', event => {
         starring: Data.Actors,
         plot: Data.Plot,
         mpaaRating: Data.Rated,
-        body: document.getElementById('postBody').value,
+        body: document.getElementById('postBody').value, // needs to be defined earlier
         userId: 1
       }
       // next 8 lines refer to creating button links to the post for slected movies as well as a button for creating a post for movies that don't already have one
@@ -155,7 +150,7 @@ document.getElementById('searchBtn').addEventListener('click', event => {
       //   document.getElementById('movieCard').append(addPost)
       //   }
     })
-    .then(() => {
+    .then(() => { // do not do
       postToDB(post)
     })
     .catch(e => console.log(e))
